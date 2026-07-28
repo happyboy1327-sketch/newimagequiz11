67caa26120107c4d6eca6c11e6033900750ef880
happyboy1327-sketch <happyboy1327@gmail.com>
2026-07-28 13:52:56 +0900

    Update server.js

--- server.js
+++ server.js
@@ -380,7 +380,6 @@ async function fillCache() {
                                 action: "query",
                                 titles: batch.join("|"),
                                 prop: "extracts|pageimages",
-                                exintro: 1,
                                 explaintext: 1,   
                                 redirects: 1,
                                 pithumbsize: 800,
