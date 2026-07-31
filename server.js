3f08b72945738ceedaf651dcdf5925a4ae60d823
happyboy1327-sketch <happyboy1327@gmail.com>
2026-07-31 12:27:37 +0900

    Increase timeout from 39000 to 46000 ms

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const WIKI_AXIOS_CONFIG = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
         'Accept': 'application/json'
     },
-    timeout: 39000 
+    timeout: 46000 
 };
 
 // VIP 풀
