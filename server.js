e3ec661455b32491f6946e723263cbaf7c10ad89
happyboy1327-sketch <happyboy1327@gmail.com>
2026-09-06 17:44:21 +0900

    Refactor buildDescription parameters in server.js
    Update buildDescription call to match new signature.

--- server.js
+++ server.js
@@ -481,13 +481,11 @@ async function fillCache() {
   
                            // 4. 개작된 summarizer.js 시그니처(introText, bodyText, aliases, maxLength)에 맞춰 연동
                            const finalDescription = buildDescription(
-                            cleanIntro, 
-                            cleanExtract, 
-                            aliases, 
-                            3,   
-                            150, 
-                            720  
-                        );
+                               cleanIntro, 
+                               cleanExtract,
+                               aliases || [], 
+                               720 // maxLength (최대 글자 수)
+                           );
                            if (finalDescription) {
                                QUIZ_CACHE.push({
                                    name: pageData.title,
